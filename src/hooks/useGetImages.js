e6dbe2cb097dbedfb1169979cec0786c8daddc08
lhw1847 <lhw1847@gmail.com>
2022-06-08 16:13:32 +0900

    0608

--- src/hooks/useGetImages.js
+++ src/hooks/useGetImages.js
@@ -32,6 +32,7 @@ const useGetImages = gameOptions => {
     useEffect(() => {
         if (!gameOptions) return;
         fetchPics();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [gameOptions]);
 
     return images;
